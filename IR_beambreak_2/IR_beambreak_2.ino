6f4544b276bd39182957a2f1c7a811fb48c9181f
marburyvthesea <jjm2128@gmail.com>
2019-10-04 12:59:21 -0500

    add

---
+++ IR_beambreak_2/IR_beambreak_2.ino
@@ -0,0 +1,52 @@
+/* 
+  IR Breakbeam sensor demo!
+*/
+
+#define LEDPIN 13
+  // Pin 13: Arduino has an LED connected on pin 13
+  // Pin 11: Teensy 2.0 has the LED on pin 11
+  // Pin  6: Teensy++ 2.0 has the LED on pin 6
+  // Pin 13: Teensy 3.0 has the LED on pin 13
+
+#define SENSORPIN 4
+#define SENSORPIN_2 5
+
+// variables will change:
+int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(LEDPIN, OUTPUT);      
+  // initialize sensor pin 1 as an input:
+  pinMode(SENSORPIN, INPUT);     
+  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  // pin 2
+  pinMode(SENSORPIN_2, INPUT);     
+  digitalWrite(SENSORPIN_2, HIGH); // turn on the pullup
+  
+  Serial.begin(9600);
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+
+  // check if the sensor beam is broken
+  // if it is, the sensorState is LOW:
+  if (sensorState == LOW) {     
+    // turn LED on:
+    digitalWrite(LEDPIN, HIGH);  
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(LEDPIN, LOW); 
+  }
+  
+  if (sensorState && !lastState) {
+    Serial.println("Unbroken");
+  } 
+  if (!sensorState && lastState) {
+    Serial.println("Broken");
+  }
+  lastState = sensorState;
+}
